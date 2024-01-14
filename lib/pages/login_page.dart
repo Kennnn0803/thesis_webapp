099add9b46f4f834027ceb900b8b3a36ec0850cb
Aaron Kenn Humbert Bondad <aaron.kenn.bondad@gmail.com>
2024-01-14 14:23:24 +0800

    firebase ui button in reusable widgets

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,4 +1,6 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:thesis_webapp/pages/home_screen.dart';
 import 'package:thesis_webapp/pages/signup_screen.dart';
 import 'package:thesis_webapp/reusable_widgets/reusable_widgets.dart';
 
@@ -8,10 +10,10 @@ class LogInScreen extends StatefulWidget {
   @override
   _LogInScreenState createState() => _LogInScreenState();
 }
-
 class _LogInScreenState extends State<LogInScreen> {
   TextEditingController _passwordTextController = TextEditingController();
   TextEditingController _emailTextController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -28,17 +30,70 @@ class _LogInScreenState extends State<LogInScreen> {
                 const SizedBox(
                   height: 50,
                 ),
-                reusableTextField("Enter Username", Icons.person_outline, false,
-                    _emailTextController),
+                reusableTextField(
+                  "Enter Username",
+                  Icons.person_outline,
+                  false,
+                  _emailTextController,
+                ),
                 const SizedBox(
                   height: 20,
                 ),
-                reusableTextField("Enter Password", Icons.lock_outline, true,
-                    _passwordTextController),
+                reusableTextField(
+                  "Enter Password",
+                  Icons.lock_outline,
+                  true,
+                  _passwordTextController,
+                ),
                 const SizedBox(
                   height: 20,
                 ),
-                signInSignUpButton(context, true, () {}),
+                firebaseUIButton(context, "Sign In", () {
+                  FirebaseAuth.instance
+                      .signInWithEmailAndPassword(
+                        email: _emailTextController.text,
+                        password: _passwordTextController.text,
+                      )
+                      .then((value) {
+                        // Successful login
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => HomeScreen()),
+                        );
+                      })
+                      .catchError((error) {
+                        // Handle login errors
+                        String errorMessage = "An error occurred during login.";
+
+                        // Check specific error codes and customize error messages
+                        if (error.code == 'user-not-found') {
+                          errorMessage = 'Email not registered!';
+                        } else if (error.code == 'wrong-password') {
+                          errorMessage = 'Wrong Password';
+                        }
+
+                        // Show error message using a pop-up or other UI element
+                        showDialog(
+                          context: context,
+                          builder: (BuildContext context) {
+                            return AlertDialog(
+                              title: Text("Login Error"),
+                              content: Text(errorMessage),
+                              actions: [
+                                TextButton(
+                                  child: Text("OK"),
+                                  onPressed: () {
+                                    Navigator.of(context).pop();
+                                  },
+                                ),
+                              ],
+                            );
+                          },
+                        );
+
+                        print("Error ${error.toString()}");
+                      });
+                }),
                 signUpOption()
               ],
             ),
@@ -47,6 +102,8 @@ class _LogInScreenState extends State<LogInScreen> {
       ),
     );
   }
+}
+
 
   Row signUpOption() {
     return Row(mainAxisAlignment: MainAxisAlignment.center, children: [
